a334a3611862cbb9c710f266014935c021476348
shaikhaalbuflasa <shaikhasaad86@gmail.com>
2022-10-16 10:15:26 +0300

    Favorite Animals

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //import express
 const express = require ('express')
+const ejsLayouts = require('express-ejs-layouts');
 
 //create an instance of express
 const app = express()
@@ -7,6 +8,18 @@ const PORT = 3000
 
 //MIDDLEWARE
 app.set('view engine','ejs')
+app.use(ejsLayouts)
+
+//HOME route
+app.get("/", (req,res)=>{
+    res.render('home.ejs')
+})
+
+//favorite animals
+app.get("/animals", (req,res)=>{
+    const faveAnimals =['cat','dog','bunnies']
+    res.render('animals.ejs', {animals: faveAnimals})
+})
 
 
 
